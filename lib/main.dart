e324d0670de8ba9ac17a5131d718ff8ca63c484c
seojin kim <001106ksj@gmail.com>
2021-07-30 09:01:54 +0900

    [CHORE] 첫 커밋
    * 세팅
    * main.dart는 공식 사이트 예제 사용

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Demo'),
+        ),
+        body: Center(
+          child: Text('Welcome Flutter'),
+        ),
+      ),
+    );
+  }
+}
